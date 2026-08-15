81c204b5d4b32ef3abd7c835ab040301c76c5b16
straimy <vrs.my.mail@gmail.com>
2026-08-15 21:26:06 +0300

    Polish Gunner Arena UI palette

---
+++ hotfix/ui/UiTheme.java
@@ -0,0 +1,18 @@
+package arena.client.ui;
+
+public final class UiTheme {
+    private UiTheme() {}
+    public static final int BACKDROP = 0xE6090C18;
+    public static final int PANEL = 0xE0141728;
+    public static final int PANEL_BORDER = 0xCC63D9FF;
+    public static final int TEXT = 0xFFF6F8FF;
+    public static final int MUTED = 0xFFA4AEC4;
+    public static final int ACCENT = 0xFF67E8FF;
+    public static final int ACCENT_2 = 0xFFC08CFF;
+    public static final int PINK = 0xFFFF78C8;
+    public static final int BLUE = 0xFF6DA7FF;
+    public static final int GREEN = 0xFF71F6B5;
+    public static final int GOLD = 0xFFFFD36D;
+    public static final int BUTTON = 0xDD17243B;
+    public static final int BUTTON_HOVER = 0xEE2A3F68;
+}
