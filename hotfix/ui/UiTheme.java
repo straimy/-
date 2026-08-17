06a057148d9bf8b193a2e590053a0acc597f9bf3
straimy <vrs.my.mail@gmail.com>
2026-08-17 03:21:52 +0300

    ui: brighten neon theme accents

--- hotfix/ui/UiTheme.java
+++ hotfix/ui/UiTheme.java
@@ -2,21 +2,21 @@ package arena.client.ui;
 
 public final class UiTheme {
     private UiTheme() {}
-    public static final int BACKDROP = 0xEC080B14;
-    public static final int PANEL = 0xED111725;
-    public static final int PANEL_SOFT = 0xC9141D2D;
-    public static final int PANEL_BORDER = 0xB85FCBE8;
-    public static final int TEXT = 0xFFF4F7FF;
-    public static final int MUTED = 0xFF8E9AB2;
-    public static final int DIM = 0xFF667289;
-    public static final int ACCENT = 0xFF6BE7E3;
-    public static final int ACCENT_2 = 0xFFB79BFF;
-    public static final int PINK = 0xFFF18DC5;
-    public static final int BLUE = 0xFF76A9EE;
-    public static final int GREEN = 0xFF7BE0AE;
-    public static final int GOLD = 0xFFE7C77A;
-    public static final int BUTTON = 0xE2162234;
-    public static final int BUTTON_HOVER = 0xF0243550;
-    public static final int HAIRLINE = 0x705E718F;
-    public static final int GLOW = 0x386BE7E3;
+    public static final int BACKDROP = 0xEA070A12;
+    public static final int PANEL = 0xF0101726;
+    public static final int PANEL_SOFT = 0xD5152135;
+    public static final int PANEL_BORDER = 0xD46CEAFF;
+    public static final int TEXT = 0xFFFFFFFF;
+    public static final int MUTED = 0xFFABB9D2;
+    public static final int DIM = 0xFF74839F;
+    public static final int ACCENT = 0xFF66FFF2;
+    public static final int ACCENT_2 = 0xFFC1A4FF;
+    public static final int PINK = 0xFFFF8ED1;
+    public static final int BLUE = 0xFF82B7FF;
+    public static final int GREEN = 0xFF80F7BD;
+    public static final int GOLD = 0xFFFFD985;
+    public static final int BUTTON = 0xEC17253A;
+    public static final int BUTTON_HOVER = 0xFF28486B;
+    public static final int HAIRLINE = 0x926C86AA;
+    public static final int GLOW = 0x5966FFF2;
 }
