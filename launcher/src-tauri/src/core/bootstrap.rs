bcc5c1db428ed9ee0bcdcd829786653d0461d93b
straimy <vrs.my.mail@gmail.com>
2026-08-17 04:41:19 +0300

    feat(launcher): target GunGloryOnline v34

--- launcher/src-tauri/src/core/bootstrap.rs
+++ launcher/src-tauri/src/core/bootstrap.rs
@@ -14,7 +14,7 @@ impl BootstrapInfo {
     pub fn current() -> Self {
         Self {
             launcher_version: env!("CARGO_PKG_VERSION"),
-            game_version: "v0.4 Beta",
+            game_version: "v34",
             channel: "beta",
             runtime: "minecraft-forge",
             server: "31.77.232.254:24842",
