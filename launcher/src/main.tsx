82ffc6a97e72779f08b6a1a62df829ebbeb41fb8
straimy <vrs.my.mail@gmail.com>
2026-08-21 01:09:50 +0300

    launcher: enable visual polish layer

--- launcher/src/main.tsx
+++ launcher/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./polish.css";
 import "./training.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
