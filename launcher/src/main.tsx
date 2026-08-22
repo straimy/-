51656b45392a9051b1174a3fc86e8e9850fbb7dd
straimy <vrs.my.mail@gmail.com>
2026-08-22 15:34:27 +0300

    feat(launcher): mount launcher update gate

--- launcher/src/main.tsx
+++ launcher/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LauncherUpdateGate from "./LauncherUpdateGate";
 import "./styles.css";
 import "./polish.css";
 import "./training.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <LauncherUpdateGate>
+      <App />
+    </LauncherUpdateGate>
   </React.StrictMode>
 );
